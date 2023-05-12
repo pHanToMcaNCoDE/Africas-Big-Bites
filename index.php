fa772a73249124c70dc4493e389c5ad3d800e77d
Victory Odumeh <113244998+pHanToMcaNCoDE@users.noreply.github.com>
2023-05-12 11:25:05 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -3,222 +3,226 @@
 
  require("nav.php");
 
+//  require('login.php');
 
 
 
 
 ?>
 
+ <!DOCTYPE html>
+ <html lang="en">
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Grosses Bouchées - Bienvenu</title>
- <link type="stylesheet" rel="style.css">
-  <script src="https://kit.fontawesome.com/032421aa45.js" crossorigin="anonymous"></script>
-</head>
-<body>
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale= 1, user-scalable=0">
+     <title>Grosses Bouchées - Bienvenu</title>
+     <script src=" https://kit.fontawesome.com/032421aa45.js" crossorigin="anonymous"></script>
 
+     <link href="css/style.css" rel="stylesheet" />
+ </head>
 
-  <!-- Hero Section -->
-  <section id="hero" class="hero">
-    <div class="max-width">
-      <div class="h-c">
-        <h1>Grosses Bouchées</h1>
-        <p>hommes affamés rendus heureux</p>
-        <div class="btn"><a href="reservations.php">Réservations</a></div>
-        <div class="down">
-          <i class="fa-solid fa-angle-down"></i>
-        </div>
-      </div>
-      <div class="locations">
-        <div class="content">
-            <div class="loc" id="one">
-              <i class="fa-solid fa-location-dot"></i>
-              <div class="txt">
-                <p>Lorem ipsum</p>
-              </div>
-            </div>
-            <div class="loc" id="two">
-              <i class="fa-solid fa-location-dot" id="l-2"></i>
-              <div class="txt">
-                <p>Lorem ipsum</p>
-              </div>
-            </div>
-            <div class="loc" id="three">
-              <i class="fa-solid fa-location-dot" id="l-3"></i>
-              <div class="txt">
-                <p>Lorem ipsum</p>
-              </div>
-            </div>
-            <div class="loc" id="four">
-              <i class="fa-solid fa-location-dot" id="l-4"></i>
-              <div class="txt">
-                <p>Lorem ipsum</p>
-              </div>
-            </div>
-        </div>
-      </div>
-    </div>
-  </section>
+ <body>
 
 
-  
-  <!-- Hours Section -->
+     <!-- Hero Section -->
+     <section id="hero" class="hero">
+         <div class="max-width">
+             <div class="h-c">
+                 <h1>Africa's Big Bites</h1>
+                 <p>where hungry men are made happy</p>
+                 <div class="btn"><a href="Reservation.php">Reservations</a></div>
+                 <div class="down">
+                     <a href="#hrs" style="color: white;"><i class="fa-solid fa-angle-down"></i></a>
+                 </div>
+             </div>
+             <div class="locations">
+                 <div class="content">
+                     <div class="loc" id="one">
+                         <i class="fa-solid fa-location-dot"></i>
+                         <div class="txt">
+                             <p>Abuja, Nigeria</p>
+                         </div>
+                     </div>
+                     <div class="loc" id="two">
+                         <i class="fa-solid fa-location-dot" id="l-2"></i>
+                         <div class="txt">
+                             <p>Washington DC, USA</p>
+                         </div>
+                     </div>
+                     <div class="loc" id="three">
+                         <i class="fa-solid fa-location-dot" id="l-3"></i>
+                         <div class="txt">
+                             <p>Munich, Germany</p>
+                         </div>
+                     </div>
+                     <div class="loc" id="four">
+                         <i class="fa-solid fa-location-dot" id="l-4"></i>
+                         <div class="txt">
+                             <p>Paris, France</p>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+     </section>
 
 
-  <section class="hrs" id="hrs">
-    <div class="max-width">
-        <div class="head">        
-          <h1>Heures</h1>
-        </div>
-      <div class="content">
-        <div class="small-talk">
-          <p>Venez prendre une énorme bouchée sur notre menu aux heures suivantes indiquées ci-dessous. Vous pouvez faire des réservations avant les heures indiquées ci-dessous.</p>
-        </div>
-        <div class="descp">
-         
-          <div class="breakfast">
-            <h2>Période de petit-déjeuner (matin):</h2>
-            <li><p>Monday 7 - 9 GMT</p></li>
-            <li><p>Wednesday 8 - 9 GMT</p></li>
-            <li><p>Thursday 8 - 10 GMT</p></li>
-          </div>
-          <div class="line"></div>
-          <div class="lunch">
-            <h2>Brunch (spéciaux, matin):</h2>
-            <li><p>Monday 10:30 - 11:30 GMT</p></li>
-          </div>
-          <div class="line"></div>
-          <div class="lunch">
-            <h2>Période du midi (après-midi):</h2>
-            <li><p>Tuesday 1 - 3 GMT</p></li>
-            <li><p>Thursday 2 - 3 GMT</p></li>
-            <li><p>Friday 1 - 2 GMT</p></li>
-          </div>
-          <div class="line"></div>
-          <div class="dinner">
-            <h2>Période du dîner (après-midi):</h2>
-            <li><p>Monday 7 - 9 GMT</p></li>
-            <li><p>Saturday 7 - 9 GMT</p></li>
-            <li><p>Friday 8 - 10 GMT</p></li>
-            <li><p>Sunday 9 - 11 GMT</p></li>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+
+     <!-- Hours Section -->
 
 
-  <!--------------------- Menu Section ---------------------->
+     <section class="hrs" id="hrs">
+         <div class="max-width">
+             <div class="head">
+                 <h1>Hours</h1>
+             </div>
+             <div class="content">
+                 <div class="small-talk">
+                     <p>Come and grab a huge bite from our menu at the following times listed below. YOU can make
+                         reservations before the times listed below.</p>
+                 </div>
+                 <div class="descp">
+
+                     <div class="breakfast">
+                         <h2>Breakfast period (morning):</h2>
+                         <li>
+                             <p>Monday 7 - 9 GMT</p>
+                         </li>
+                         <li>
+                             <p>Wednesday 8 - 9 GMT</p>
+                         </li>
+                         <li>
+                             <p>Thursday 8 - 10 GMT</p>
+                         </li>
+                     </div>
+                     <div class="line"></div>
+                     <div class="lunch">
+                         <h2>Brunch (specials, morning):</h2>
+                         <li>
+                             <p>Monday 10:30 - 11:30 GMT</p>
+                         </li>
+                     </div>
+                     <div class="line"></div>
+                     <div class="lunch">
+                         <h2>Noon period (afternoon):</h2>
+                         <li>
+                             <p>Tuesday 1 - 3 GMT</p>
+                         </li>
+                         <li>
+                             <p>Thursday 2 - 3 GMT</p>
+                         </li>
+                         <li>
+                             <p>Friday 1 - 2 GMT</p>
+                         </li>
+                     </div>
+                     <div class="line"></div>
+                     <div class="dinner">
+                         <h2>Dinner period (afternoon):</h2>
+                         <li>
+                             <p>Monday 7 - 9 GMT</p>
+                         </li>
+                         <li>
+                             <p>Saturday 7 - 9 GMT</p>
+                         </li>
+                         <li>
+                             <p>Friday 8 - 10 GMT</p>
+                         </li>
+                         <li>
+                             <p>Sunday 9 - 11 GMT</p>
+                         </li>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </section>
 
 
-  <section class="menu" id="menu">
-    <div class="max-width">
-      <div class="content">
-        <h1>Menu</h1>
-        <div class="txt">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate.
-          Voluptatum ducimus voluptates voluptas?</p> 
-          <div class="btn">
-              <a href="#">Show menu</a> 
-          </div>
-        </div>
-      </div>
-  </section>
-
-  
+     <!--------------------- Menu Section ---------------------->
 
 
-  <!--------------------- News Letter Section ---------------------->
+     <section class="menu" id="menu">
+         <div class="max-width">
+             <div class="content">
+                 <h1>Menu</h1>
+                 <div class="txt">
+                     <p>Check out our top class cuisines and review of our loyal customers. You can also make an order
+                         at the times listed. Hit the button below!
+                     </p>
+                     <div class="btn">
+                         <a href="menu.php">Show menu</a>
+                     </div>
+                 </div>
+             </div>
+     </section>
+
+     <div class="line"></div>
 
 
-  <section class="news">
-    <div class="max-width">
-      <div class="news-letter"> 
-        <p>Pour plus d'informations ou des nouvelles sur les nouveautés et l'événement des Grosses Bouchées, inscrivez-vous à notre newsletter.</p>
-        <form action="" method="post">
-          <input type="email" name="newsletter" placeholder="Adresse e-mail">
-          <button type="submit">Soumettre</button>
-        </form>
-      </div>
-    </div>
-    </div>
-  </section>
-  <div class="line"></div>
+     <!--------------------------- Event Section ------------------------->
+
+     <section class="eve" id="eve">
+         <div class="max-width">
+             <div class="box"></div>
+             <div class="content">
+                 <div class="left">
+                     <img src="assets/menu1.jpg" alt="Food Image">
+                 </div>
+                 <div class="right">
+                     <h1>Events</h1>
+                     <p>Do you wish to join us on our up coming event?</p>
+                     <p>Then, join us this year to select new cuisines that will be added to our menu.
+                     </p>
+                     <p>Don't miss it!</p>
+                 </div>
+             </div>
+         </div>
+     </section>
 
 
-  <!--------------------------- Event Section ------------------------->
+     <!-- Contact Section -->
 
-  <section class="eve" id="eve">
-    <div class="max-width">
-       <div class="box"></div> 
-        <div  class="content">
-          <div class="left">
-            <img src="assets/menu1.jpg" alt="Food Image">
-          </div>
-          <div class="right"> 
-            <h1>Events</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p> 
-            <p>Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate.</p> 
-            <p>Voluptatum ducimus voluptates voluptas?</p>
-            <div class="btn">
-              <a href="#">Show menu</a> 
-            </div>
-          </div>
-        </div>
-      </div>
-  </section>
+     <section class="con" id="con">
+         <h1>Contact us</h1>
+         <div class="max-width">
+             <div class="content">
+                 <div class="row">
+                     <div class="head">Telephone:</div>
+                     <li>Abuja, Nigeria: +234-12-123-234</li>
+                     <li>Paris: +1-45-456-567</li>
+                     <li>USA: +00-78-789-890</li>
+                     <li>France: +3-01-012-123</li>
+                 </div>
+                 <div class="row">
+                     <div class="head">Social Media:</div>
+                     <li><a href="#">FB</a></li>
+                     <li><a href="#">IG</a></li>
+                     <li><a href="#">TW</a></li>
+                     <li><a href="#">LN</a></li>
+                 </div>
 
+                 <div class="row">
+                     <div class="head">Locations:</div>
+                     <li>Paris, France</li>
+                     <li>Abuja, Nigeria</li>
+                     <li>Washington DC, USA</li>
+                     <li>Munich, Germany</li>
+                 </div>
 
-      <!-- Contact Section -->
+                 <div class="row">
+                     <div class="head">E-mail:</div>
+                     <li>team.abb@gmail.com</li>
+                 </div>
+             </div>
+         </div>
+     </section>
+     <div class="bottom-line"></div>
 
-      <section class="con" id="con">
-          <h1>Contact us</h1>
-        <div class="max-width">
-          <div class="content">
-                <div class="row">
-                  <div class="head">Telephone:</div>
-                  <li>lorem: +1-12-123-234</li>
-                  <li>lorem: +1-45-456-567</li>
-                  <li>lorem: +1-78-789-890</li>
-                  <li>lorem: +1-01-012-123</li>
-                </div>
-
-                
-                <div class="row">
-                  <div class="head">Social Media:</div>
-                  <li><a href="#">FB</a></li>
-                  <li><a href="#">IG</a></li>
-                  <li><a href="#">TW</a></li>
-                  <li><a href="#">LN</a></li>
-                </div>
-              
-                <div class="row">
-                  <div class="head">Locations:</div>
-                  <li>lorem ipsum</li>
-                  <li>lorem ipsum</li>
-                  <li>lorem ipsum</li>
-                  <li>lorem ipsum</li>
-                </div>
-
-                <div class="row">
-                  <div class="head">E-mail:</div>
-                  <li>Lorem ipsum</li>
-                </div>
-            </div>
-          </div>
-      </section>
-      <div class="bottom-line"></div>
-
-      <?php
+     <?php
 
         require("footer.php");
       ?>
 
-</body>
-</html> 
+ </body>
+
+ </html>
